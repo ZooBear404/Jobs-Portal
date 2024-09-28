90b5f3e8c5afbb848f9627f5fbcf848cb7bcdf21
ZooBear404 <zubairstudytech@gmail.com>
2024-09-28 18:06:44 +0430

    feat(struct): change header to use session_token

--- src/header.php
+++ src/header.php
@@ -3,6 +3,27 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="output.css">
 	<title>Document</title>
 </head>
 <body>
+	<div class="nav">
+		<div class="logo">Name</div>
+		<div class="buttons">
+			<?php
+				session_start();
+				if (!isset($_SESSION['session_token'])) {
+			?>
+				<div class="sign-up">Sign Up</div>
+				<div class="login">Login</div>
+			<?php } else {
+					require("db_config.php");
+					$session_token = $_SESSION["session_token"];
+					require("JobSeekerDatabase.php");
+					$id = JobSeekerDatabase\getUserIdFromSessionToken($session_token);
+					$image_url = JobSeekerDatabase\getUserImageUrlFromId($id);
+
+					echo "<img src='static/images/profiles/$image_url' alt='P' height='50px'>";
+			 } ?>
+		</div>
+	</div>
