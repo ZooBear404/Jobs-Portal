e7b55c37e512f332b1e098b83d7238c80410e4ac
ZooBear404 <zubairstudytech@gmail.com>
2024-09-28 06:46:31 +0430

    feat: add index home page

---
+++ index.php
@@ -0,0 +1,16 @@
+-- Active: 1726567062500@@127.0.0.1@3306@job_portal
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./src/output.css">
+	<title>Homepage</title>
+</head>
+<body>
+	<div class="page-container">
+		<nav class="flex justify-between p-4"><div>Name</div><div><button>Sign up</button><button>Login</button></div></nav>
+		<div></div>
+	</div>
+</body>
+</html>
