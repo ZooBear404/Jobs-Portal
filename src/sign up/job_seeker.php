5aa0709ab814b19d633efcc540a92c5e42ddcfee
ZooBear404 <zubairstudytech@gmail.com>
2024-09-28 12:12:20 +0430

    feat(struct): create sign up page for job seeker

--- src/sign up/job_seeker.php
+++ src/sign up/job_seeker.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../../output.css">
+	<link rel="stylesheet" href="/output.css">
 	<title>Job Seeker Sign Up</title>
 </head>
 <body>
@@ -22,10 +22,71 @@
 				<div class="login-button">Login</div>
 			</div>
 			<div class="sign-up-form">
-				
+				<div class="sign-up-title">
+					<p class="title-create-account">Create an Account</p>
+				</div>
+				<div>
+					<form action="" method="post">
+						<div class="div-first_name">
+							<label for="first_name_input">First Name</label>
+							<input type="text" name="first_name" id="first_name_input" required>
+						</div>
+						<div class="div-last_name">
+							<label for="last_name_input">Last Name</label>
+							<input type="text" name="last_name" id="last_name_input" required>
+						</div>
+						<div class="div-email">
+							<label for="email_input">Email</label>
+							<input type="email" name="email" id="email_input" required>
+						</div>
+						<div class="div-password">
+							<label for="password_input">Password</label>
+							<input type="password" name="password" id="password_input" required>
+						</div>
+						<div class="div-birth">
+							<label for="date_of_birth_input">Date of Birth</label>
+							<input type="date" name="birth_year">
+						</div>
+						<div class="gender">
+							<label for="gender">Gender</label>
+							<input type="radio" name="gender" value="M" id="M-input">
+							<label for="M-Input">Male</label>
+							<input type="radio" name="gender" value="F" id="F-input">
+							<label for="F-input">Female</label>
+						</div>
+						<div class="div-submit">
+							<input type="submit">
+						</div>
+						<?php
+							use function JobSeekerDatabase\signUpJobSeeker;
 
+							if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+								$first_name = $_POST['first_name'];
+								$last_name = $_POST['last_name'];
+								$email = $_POST['email'];
+								$password = $_POST['password'];
+								$gender = $_POST['gender'];
+								$date_of_birth = $_POST['birth_year'];
+
+								echo "<pre>
+									'$first_name', '$last_name', '$email', '$password', '$gender', '$date_of_birth'
+								</pre>";
+
+								require "../JobSeekerDatabase.php";
+								$result = JobSeekerDatabase\signUpJobSeeker($first_name, $last_name, $email, $gender, $date_of_birth, $password);
+								if ($result) {
+									echo "<p class='success-text'>You Successfully Signed Up</p>";
+								} else {
+									echo "<p class='failure-text'>Something went wrong!!!</p>";
+								}
+
+							}
+						?>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
 </body>
 </html>
+
