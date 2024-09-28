5ec0ce06e9803f3e152055ddd24497fa00d4f14f
ZooBear404 <zubairstudytech@gmail.com>
2024-09-28 09:31:33 +0430

    feat(struct): remove src/pages directory
    sign up page for job seeker

---
+++ src/sign up/job_seeker.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="../../output.css">
+	<title>Job Seeker Sign Up</title>
+</head>
+<body>
+	<div class="container">
+		<div class="right">
+			<div class="logo">Name</div>
+			<div class="title">Job Seeker</div>
+			<div class="footer">
+				<div class="admin-login"><a href="admin-login.php">Admin</a></div>
+				<div class="review">Review from userReview from users</div>
+			</div>
+
+		</div>
+		<div class="left">
+			<div class="login">
+				<div class="login-button">Login</div>
+			</div>
+			<div class="sign-up-form">
+				
+
+			</div>
+		</div>
+	</div>
+</body>
+</html>
