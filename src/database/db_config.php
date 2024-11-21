65ce2c0eb7895c60800e8f333f36798968a5e12b
ZooBear404 <zubairstudytech@gmail.com>
2024-11-21 10:53:22 +0430

    database(connection): fix connection error

--- src/database/db_config.php
+++ src/database/db_config.php
@@ -3,10 +3,8 @@
 define("hostname", "localhost");
 define("username", "job");
 define("dbname", "job_portal");
-
 require("passwords.php");
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $con = new mysqli(hostname, username, password, dbname);
 if ($con->connect_error) {
 	die("". $con->connect_error);
